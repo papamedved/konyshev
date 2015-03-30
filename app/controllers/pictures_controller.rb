9507ce1d6840ce5feee0dae4221de5efa054f915
Sergey <itfender@gmail.com>
2015-03-30 13:03:17 +0400

    add devise

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /pictures
   # GET /pictures.json
